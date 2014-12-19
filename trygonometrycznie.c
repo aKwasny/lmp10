110976ca930bc7821ffc763df5cc3fe1cc0a77f0
Andrzej Kwasiborski <andrzej.kwasiborski@gmail.com>
2014-12-19 11:17:29 +0100

    Poważne rozpoczęcie prac nad aproksymatorem trygonometrycznym. Utworzenie podstawowego pliku trygonometryczny.c

---
+++ trygonometrycznie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+/* na podstawie http://kik.weii.tu.koszalin.pl/aproksymacja/aproks_tryg/page_dyp/aproksytr1.htm */
+
+void czytaj () {
+	FILE *in = argc > 1 ? fopen(argv[1], "r") : stdin;
+	
+}
+
+
+int main () {
+	czytaj ();
+	
+}
