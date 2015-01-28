45e09ef34d9fbea25fc8494f4a6a7cb6b3655b08
Andrzej Kwasiborski <andrzej.kwasiborski@gmail.com>
2015-01-28 14:32:21 +0100

    Działa wszystko. Projekt piękny i gładki.

--- trygonometrycznie/najnowsze_trygonometrycznie.c
+++ trygonometrycznie/najnowsze_trygonometrycznie.c
@@ -8,16 +8,10 @@
 /* na podstawie http://kik.weii.tu.koszalin.pl/aproksymacja/aproks_tryg/page_dyp/aproksytr1.htm */
 
 
-#define PI 3.14159265
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 
 
-/*int maxm (int n) { 
-	if (n % 2 == 1) {
-		return (n-1)/2-1;
-	} else {
-		return n/2-1;
-	}
-}*/
+
 
 double *licz_a(points_t pts) {
 	int n = pts.n;
@@ -50,7 +44,7 @@ double *licz_a(points_t pts) {
 			wyn = wyn / n;
 
 			a[i] = wyn;
-		} else {
+		} else {				/* ustalanie kolejnych wartości ai */
 
 			for(j = 1; j <= n; j++) {
 				wyn += pts.y[j - 1] * cos((2 * PI * i * j)/n);
@@ -119,17 +113,18 @@ double y(points_t pts, double x, double *licz_a, double *licz_b) {			/*trzeba wy
 	double y = 0;
 	int i;
 
-	if (n % 2 == 1) {
+	/*if (n % 2 == 1) {
 		m = ((n-1)/2)-1;
 	} else {
 		m = (n/2)-1;
-	}
+	}*/
+
+	m = ((n-1)/2) - 1;
 	
 	y += licz_a[0]; /*dodanie do y wartości a0 */
 
 	for(i = 1; i <= m; i++) {
-		y += (licz_a[i] * (cos(2*PI*i/n*x))) + (licz_b[i] * (sin(2*PI*i/n*x)));
+		y += (licz_a[i] * (cos(2*PI*i/n*(x+1)))) + (licz_b[i] * (sin(2*PI*i/n*(x+1))));
 	}
-
 	return y;
 }
